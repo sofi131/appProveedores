7e3120c1e03f8ce40b88d92368210271d5ec8077
sofi131 <sofiasanmartin131@gmail.com>
2024-01-24 16:59:10 +0100

    Conectando usuarios a la base de datos de proveedores.

--- src/com/ceica/Modelos/Usuario.java
+++ src/com/ceica/Modelos/Usuario.java
@@ -2,66 +2,27 @@ package com.ceica.Modelos;
 
 import com.ceica.bbdd.Conexion;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 public class Usuario {
-    private int id;
-    private String usuario;
-    private String password;
-
-    //vacío
-    public Usuario() {
-    }
-
-    //------------------------------------------SETTER---------------------------
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-    //-----------------------------------GETTER----------------------------------
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    //------------------------------------ListaUsuarios----------------------------------
-    public List getUsuarios()  {
-        List usuarioList = new ArrayList<>();
+    public static boolean getUsuario(String usr, String pass) {
         Connection conn = Conexion.conectar();
-        String sql = "select * from usuario";
+        String sql = "select * from usuario where usuario=? and password=?";
         try {
-            Statement stm = conn.createStatement();
-            ResultSet respuesta = stm.executeQuery(sql);
-            while (respuesta.next()){
-                Usuario usuario = new Usuario();
-                usuario.setId(respuesta.getInt("idUsuario"));
-                usuario.setUsuario(respuesta.getString("nombreUsuario"));
-                usuario.setPassword(respuesta.getString("password"));
-                usuarioList.add(usuario);
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, usr);
+            pst.setString(2, pass);
+            ResultSet resultSet = pst.getResultSet();
+            if (resultSet.next()) {
+                return true;
+            } else {
+                return false;
             }
-            conn.close();
         } catch (SQLException e) {
-            // Manejo de la excepción
-            e.printStackTrace();
+            //throw new RuntimeException(e);
+            return false;
         }
-        return usuarioList;
     }
 }
 
