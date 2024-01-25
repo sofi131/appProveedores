0f587fc73c8cc5ff08966933ba2dddcde2d32650
sofi131 <sofiasanmartin131@gmail.com>
2024-01-25 18:38:14 +0100

    Haciendo un Modelo Base.

--- src/com/ceica/Modelos/ModeloBase.java
+++ src/com/ceica/Modelos/ModeloBase.java
@@ -1,18 +1,20 @@
 package com.ceica.Modelos;
 
+import java.lang.reflect.Method;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Properties;
 import java.util.List;
+import java.util.Properties;
 
 public abstract class ModeloBase {
     private static final String CONFIG_FILE = "config.properties";
     protected static String URL;
     protected static String USUARIO;
     protected static String PASSWORD;
+
 
     static {
         cargarConfiguracion();
@@ -33,39 +35,22 @@ public abstract class ModeloBase {
     // Método abstracto para obtener el nombre de la tabla
     protected abstract String getNombreTabla();
 
-    // Métodos para CRUD---- INSERT, UPDATE, DELETE, READ
+    // Métodos para CRUD
 
     protected void insertar(String sql, Object... parametros) {
-        sql="insert into " +getNombreTabla()+" "+sql;
+
+        sql = "insert into " + getNombreTabla() + " " + sql;
         ejecutarQuery(sql, parametros);
     }
 
     protected void actualizar(String sql, Object... parametros) {
-        sql="update "+getNombreTabla()+" "+sql;
+        sql = "update " + getNombreTabla() + " " + sql;
         ejecutarQuery(sql, parametros);
     }
 
     protected void borrar(String sql, Object... parametros) {
-        sql="delete from "+getNombreTabla()+" where "+sql;
+        sql = "delete from " + getNombreTabla() + " where " + sql;
         ejecutarQuery(sql, parametros);
-    }
-
-    // Método para leer datos de la base de datos -- devolver lista de obj
-    protected List<Object> leer(String sql, Object... parametros) {
-        List<Object> objectList = new ArrayList<>();
-        try {
-            Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-            PreparedStatement preparedStatement = conexion.prepareStatement(sql);
-            // Establecer los valores de los parámetros
-            for (int i = 0; i < parametros.length; i++) {
-                preparedStatement.setObject(i + 1, parametros[i]);
-            }
-            ResultSet resultSet = preparedStatement.executeQuery();
-            ResultSetMetaData md = resultSet.getMetaData();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        // Implementa la lógica para leer datos
     }
 
     // Método genérico para ejecutar consultas SQL
@@ -85,5 +70,28 @@ public abstract class ModeloBase {
             e.printStackTrace();
         }
     }
-}
 
+    protected abstract Object createObjectFromResultSet(ResultSet resultSet) throws SQLException;
+
+    protected List<Object> leerTodos() {
+        List<Object> resultList = new ArrayList<>();
+
+        String sql = "SELECT * FROM " + getNombreTabla();
+
+        try (Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+             PreparedStatement preparedStatement = conexion.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Object obj = createObjectFromResultSet(resultSet);
+                resultList.add(obj);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
+    }
+
+}
